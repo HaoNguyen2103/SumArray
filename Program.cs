3abd9c3117d701c6febe3619325aa201a8b49888
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-08-17 21:47:14 +0700

    add
    nop bai

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = {2,5,9,6,7};
+        int total = 0;
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            total += numbers[i];
+        }
+        Console.WriteLine(total);
+    }
+}
